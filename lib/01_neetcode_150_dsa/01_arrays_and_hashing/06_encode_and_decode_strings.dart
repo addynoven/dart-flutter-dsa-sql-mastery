ceb84910267d54062e3d437631a2fcca8a29bfe6
addynoven <dmcbaditya@gmail.com>
2026-09-08 17:28:46 +0530

    Implement length-prefix algorithm solution for Encode and Decode Strings [template sync]

--- lib/01_neetcode_150_dsa/01_arrays_and_hashing/06_encode_and_decode_strings.dart
+++ lib/01_neetcode_150_dsa/01_arrays_and_hashing/06_encode_and_decode_strings.dart
@@ -1,46 +1,68 @@
-/// Problem: Encode and Decode Strings
-/// Difficulty: Medium
-/// NeetCode Link: https://neetcode.io/problems/string-encode-and-decode
-/// LeetCode Link: https://leetcode.com/problems/encode-and-decode-strings
-
-import 'dart:collection';
-import 'package:collection/collection.dart';
-
-// Node structure helpers for Trees / Linked Lists
-class ListNode {
-  int val;
-  ListNode? next;
-  ListNode([this.val = 0, this.next]);
-}
-
-class TreeNode {
-  int val;
-  TreeNode? left;
-  TreeNode? right;
-  TreeNode([this.val = 0, this.left, this.right]);
-}
+/*
+Problem: Encode and Decode Strings
+Difficulty: Medium
+NeetCode Link: https://neetcode.io/problems/string-encode-and-decode
+LeetCode Link: https://leetcode.com/problems/encode-and-decode-strings
+*/
 
 class Solution {
-  /// Solution method for Encode and Decode Strings
-  dynamic encodeAndDecodeStrings(dynamic param) {
-    // TODO: Write your solution algorithm here
-    return null;
+  /// Encodes a list of strings to a single string using length-prefix encoding (length + '#' + string).
+  String encode(List<String> strs) {
+    final sb = StringBuffer();
+    for (final s in strs) {
+      sb.write('${s.length}#$s');
+    }
+    return sb.toString();
+  }
+
+  /// Decodes a single string back to a list of strings.
+  List<String> decode(String str) {
+    final res = <String>[];
+    int i = 0;
+
+    while (i < str.length) {
+      final delimiterIndex = str.indexOf('#', i);
+      if (delimiterIndex == -1) break;
+
+      final length = int.parse(str.substring(i, delimiterIndex));
+      final start = delimiterIndex + 1;
+      final end = start + length;
+
+      res.add(str.substring(start, end));
+      i = end;
+    }
+
+    return res;
+  }
+
+  /// Helper method that tests both encoding and decoding in sequence.
+  List<String> encodeAndDecodeStrings(List<String> strs) {
+    final encoded = encode(strs);
+    return decode(encoded);
   }
 }
 
 void main() {
   final solution = Solution();
   print('=== Testing: Encode and Decode Strings ===\n');
+
   // --- Example 1 ---
-  final param1 = ["neet","code","love","you"];
-  final result1 = solution.encodeAndDecodeStrings(param1);
-  print('Example 1 Result: $result1');
-  print('Expected Output:  ["neet","code","love","you"]\n');
+  final param1 = ["neet", "code", "love", "you"];
+  final encoded1 = solution.encode(param1);
+  final decoded1 = solution.decode(encoded1);
+  print('Example 1 Original: $param1');
+  print('Example 1 Encoded:  "$encoded1"');
+  print('Example 1 Decoded:  $decoded1');
+  print('Expected Output:   ["neet", "code", "love", "you"]\n');
+
   // --- Example 2 ---
-  final param2 = ["we","say",":","yes"];
-  final result2 = solution.encodeAndDecodeStrings(param2);
-  print('Example 2 Result: $result2');
-  print('Expected Output:  ["we","say",":","yes"]\n');
+  final param2 = ["we", "say", ":", "yes"];
+  final encoded2 = solution.encode(param2);
+  final decoded2 = solution.decode(encoded2);
+  print('Example 2 Original: $param2');
+  print('Example 2 Encoded:  "$encoded2"');
+  print('Example 2 Decoded:  $decoded2');
+  print('Expected Output:   ["we", "say", ":", "yes"]\n');
 }
 
 /*
